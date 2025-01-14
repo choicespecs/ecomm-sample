0e741d0f984582832fa1aec688f257f2e688d8ce
christophermlee2 <christophermlee2@gmail.com>
2025-01-13 22:08:33 -0500

    revise database

--- notification_service/init-db.sh
+++ notification_service/init-db.sh
@@ -13,10 +13,10 @@ echo "PostgreSQL is ready!"
 # Create user, database, and tables
 su - postgres -c "psql -c \"CREATE USER notification_user WITH PASSWORD 'password';\""
 su - postgres -c "psql -c \"CREATE DATABASE notification_db OWNER notification_user;\""
-su - postgres -c "psql notification_db -c \"CREATE TABLE users (
-    user_id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(255) NOT NULL
+su - postgres -c "psql notification_db -c \"CREATE TABLE notifications (
+    notification_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    message VARCHAR(255) NOT NULL
 );\""
 su - postgres -c "psql notification_db -c \"ALTER TABLE notification_db OWNER TO notification_user;\""
-su - postgres -c "psql notification_db -c \"INSERT INTO users (name, email) VALUES ('John Smith', 'john.smith@test.com'), ('David Barrow', 'david.man@example.com'), ('Diana Terry', 'dterr49@test.com');\""
+su - postgres -c "psql notification_db -c \"INSERT INTO notifications (user_id, message) VALUES (1, 'Order Confirmed'), (2, 'Order Cancelled'), (3, 'Order on the way');\""
